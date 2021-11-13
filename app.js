d143ce1d02b0660e73b3a9895db6d7224a10dbf7
rylan-silveira <54469690+rylan-silveira@users.noreply.github.com>
2021-11-13 15:24:47 +0400

    commit 2

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require("express");
 //creating app
 const app = express();
-//send the index.html when receiving HTTP GET /
+app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.sendFile("index.html", { root: __dirname });
 });
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`Cart app listening at http://localhost:${port}`);
+});
+
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("index"); //no need for ejs extension
+});
+
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
 });
